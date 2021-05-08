0ca947f0dea62a419e67351869042d471dffa774
Andreas Richter <andreas.richter@groupon.com>
2021-05-08 12:06:46 +0200

    test: fix expire test

--- test/get-set.test.js
+++ test/get-set.test.js
@@ -43,12 +43,18 @@ describe('Cache::{get,set,unset}', () => {
       assert.strictEqual(await cache.get('promise-key'), null);
     });
 
+    it('getting an unknown key returns null', async () => {
+      assert.strictEqual(await cache.get('unknown'), null);
+    });
+
     it('honors expires', async () => {
       const values = {
         key1: 'Value 1',
         key2: 'Value 2',
         key3: 'Value 3',
       };
+
+      await Promise.all(['key1', 'key2', 'key3'].map(key => cache.unset(key)));
 
       await Promise.all([
         cache.set('key1', values.key1, { expire: 1 }),
@@ -59,7 +65,7 @@ describe('Cache::{get,set,unset}', () => {
       await delay(2000);
 
       const [expired, eternal, hit] = await Promise.all(
-        ['key', 'key2', 'key3'].map(key => cache.get(key))
+        ['key1', 'key2', 'key3'].map(key => cache.get(key))
       );
 
       assert.strictEqual(expired, null);
